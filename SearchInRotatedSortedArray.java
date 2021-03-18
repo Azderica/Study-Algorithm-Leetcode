081e549c527f4a22a97d8322588428b2a2e4221e
Myeonghun Park <42582516+Azderica@users.noreply.github.com>
2021-03-19 07:27:21 +0900

    Create SearchInRotatedSortedArray.java

---
+++ SearchInRotatedSortedArray.java
@@ -0,0 +1,37 @@
+package com.myepark;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+        System.out.println(search(new int[]{4, 5, 6, 7, 0, 1, 2}, 3));
+        System.out.println(search(new int[]{1}, 0));
+        System.out.println(search(new int[]{5, 6, 7, 8, 9, 10, 1, 2, 3,}, 3));
+        System.out.println(search(new int[]{30, 40, 50, 10, 20}, 10));
+    }
+
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == target)
+                return mid;
+            if(nums[left] <= nums[mid]) {
+                if(nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            } else {
+                if(nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
