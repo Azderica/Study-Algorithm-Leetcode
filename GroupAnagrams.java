f8c498610cd75ff18a7aad72f4598cecbe33dc9d
Myeonghun Park <42582516+Azderica@users.noreply.github.com>
2021-04-20 08:25:38 +0900

    Create GroupAnagrams.java

---
+++ GroupAnagrams.java
@@ -0,0 +1,52 @@
+package com.myepark;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution {
+
+    public static void output(List<List<String>> res) {
+        for (List<String> list : res) {
+            System.out.print("[");
+            for (String val : list) {
+                System.out.print(val + " ");
+            }
+            System.out.print("], ");
+        }
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        output(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+        output(groupAnagrams(new String[]{""}));
+        output(groupAnagrams(new String[]{"a"}));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        List<List<String>> res = new ArrayList<>();
+
+        for(String str : strs) {
+            // make key
+            char[] strChars = str.toCharArray();
+            Arrays.sort(strChars);
+            String key = new String(strChars);
+
+            if(!map.containsKey(key)) {
+                List<String> set = new ArrayList<>();
+                set.add(str);
+                map.put(key, set);
+            } else {
+                map.get(key).add(str);
+            }
+        }
+
+        map.forEach((key, value) -> {
+            res.add(value);
+        });
+
+        return res;
+    }
+}
