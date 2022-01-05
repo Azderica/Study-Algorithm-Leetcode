65231f3e15d2baa7624c468e5b59d314c4ae8b80
Azderica <mh97888@gmail.com>
2022-01-05 22:11:22 +0900

    leetcode sort colors

---
+++ leetcode/from51to100/SortColors.java
@@ -0,0 +1,20 @@
+package com.myepark.leetcode.from51to100;
+
+public class SortColors {
+    public void sortColors(int[] nums) {
+        // init
+        int[] colors = new int[3];
+
+        // main logic -> count sort
+        for(int num : nums) {
+            colors[num]++;
+        }
+
+        // make answer
+        int index = 0;
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < colors[i]; j++) {
+                nums[index++] = i;
+            }
+        }
+}
